2daa22f39dc52dc613cfbac30db956a3d6aebd98
dking1115 <davidking@amdg.rockhursths.edu>
2020-02-10 10:47:33 -0600

    more efficent

--- sim.py
+++ sim.py
@@ -9,4 +9,4 @@ for i in range(1,outof+1):
     for j in range(0,len(vals)):
         if(vals[j]==i):
             c+=1
-    print(f"{i+1}:{c}/{rolls}={100*(c/rolls)}%")
+    print(f"{i}:{c}/{rolls}={100*(c/rolls)}%")
