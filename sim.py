8968cc0a98effee690951235b034681eed7401b0
dking1115 <davidking@amdg.rockhursths.edu>
2020-02-10 09:38:51 -0600

    more efficent

--- sim.py
+++ sim.py
@@ -1,28 +1,12 @@
 import random
-one=0
-two=0
-three=0
-four=0
-five=0
-six=0
+vals=[]
+outof=6
 rolls=int(input("howmany rolls"))
 for roll in range(0,rolls):
-    num=random.randint(1,6)
-    if(num==1):
-        one+=1
-    elif(num==2):
-        two+=1
-    elif(num==3):
-        three+=1
-    elif(num==4):
-        four+=1
-    elif(num==5):
-        five+=1
-    elif(num==6):
-        six+=1
-print(f"1:{one}/{rolls}={100*(one/rolls)}%")
-print(f"2:{two}/{rolls}={100*(two/rolls)}%")
-print(f"3:{three}/{rolls}={100*(three/rolls)}%")
-print(f"4:{four}/{rolls}={100*(four/rolls)}%")
-print(f"5:{five}/{rolls}={100*(five/rolls)}%")
-print(f"6:{six}/{rolls}={100*(six/rolls)}%")
+    vals.append(random.randint(1,outof))
+for i in range(1,outof+1):
+    c=0
+    for j in range(0,len(vals)):
+        if(vals[j]==i):
+            c+=1
+    print(f"{i+1}:{c}/{rolls}={100*(c/rolls)}%")
