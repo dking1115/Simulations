85f57105ece7c2464e549bf746dc1c5acc983e0c
dking1115 <davidking@amdg.rockhursths.edu>
2020-02-12 08:56:41 -0600

    got part A done

--- coins.py
+++ coins.py
@@ -5,17 +5,21 @@ c=0
 r=[]
 hr=0
 tr=0
+run=0
+ser=int(input("Which side are you looking for (0-heads, 1-tails)?"))
+numrun=int(input("How long is the run you're looking for?"))
 for i in range(0,numflips):
     flips.append(random.randint(0,1))
     
 for j in range(0,len(flips)):
     c+=flips[j]
-    if(flips[j]==flips[j-1]):
-        if(flips[j]==0):
-            tr+=1
-        else:
-            hr+=1
+    if(j>numrun-1):
+        ru=0
+        for g in range(j-numrun,j):
+            if(flips[g]==ser):
+                ru+=1
+            if(ru>=numrun):
+                run+=1
 print(f"heads:{c} {100*c/numflips}")
 print(f"tails:{numflips-c} {100*(numflips-c)/numflips}")
-print(f"{hr} runs of Heads")
-print(f"{tr} runs of Tails")
+print(f"{run} runs of {numrun}")
