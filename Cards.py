a2cd617b6f02743c8edbe919dd35a4eef0f02a87
dking1115 <davidking@amdg.rockhursths.edu>
2020-02-18 08:44:17 -0600

    Recursive shuffling for better random

--- Cards.py
+++ Cards.py
@@ -1,15 +1,20 @@
 import random
+def Shuffle(array):
+    for a in range(len(array)-2):
+        g=random.randint(a,(len(array)-1))
+        b=array[a]
+        array[a]=array[g]
+        array[g]=b
+    return(array)
+    
 deck =[]
 suits=["Spades","Hearts","Diamonds","Clubs"]
 cards=["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"]
 for i in suits:
     for j in cards:
         deck.append(f"{j} of {i}")
-
-for a in range(len(deck)-2):
-    g=random.randint(a,(len(deck)-1))
-    b=deck[a]
-    deck[a]=deck[g]
-    deck[g]=b
-print(deck)
+for i in range(10):
+    deck=Shuffle(deck)
     
+for d in deck:
+    print(d)
