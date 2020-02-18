a889d1fe26d0a15d75386106a984d830fed304c1
dking1115 <davidking@amdg.rockhursths.edu>
2020-02-18 08:33:17 -0600

    Shuffled Deck

--- Cards.py
+++ Cards.py
@@ -1,7 +1,15 @@
+import random
 deck =[]
 suits=["Spades","Hearts","Diamonds","Clubs"]
 cards=["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"]
 for i in suits:
     for j in cards:
         deck.append(f"{j} of {i}")
+
+for a in range(len(deck)-2):
+    g=random.randint(a,(len(deck)-1))
+    b=deck[a]
+    deck[a]=deck[g]
+    deck[g]=b
 print(deck)
+    
