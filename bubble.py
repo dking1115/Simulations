fc847a153db89965f5fb835a0ff328b8f8747927
dking1115 <davidking1115@me.com>
2020-02-19 08:38:03 -0600

    bubbble sort testing

---
+++ bubble.py
@@ -0,0 +1,25 @@
+def bubbleSort(arr):
+    n = len(arr)
+ 
+    # Traverse through all array elements
+    for i in range(n):
+ 
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+ 
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j+1] :
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    print(arr)
+
+arr1 = [64, 34, 25, 12, 22, 11, 90]
+arr2 = [10,100,5]
+arr3 = [3,10]
+arr4 = []
+
+bubbleSort(arr1)
+bubbleSort(arr2)
+bubbleSort(arr3)
+bubbleSort(arr4)
